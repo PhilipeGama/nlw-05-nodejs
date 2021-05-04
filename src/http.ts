3286127e939efd71a57b096e2c07522cdc645831
PhilipeGama <philipegama97@gmail.com>
2021-05-04 07:49:03 -0400

    aula 5 finalizada

--- src/http.ts
+++ src/http.ts
@@ -1,33 +1,35 @@
-import express, { request, response } from "express";
+import express from "express";
 import { createServer } from "http";
 import { Server, Socket } from "socket.io";
+import path from "path";
 
 import "./database";
 import { routes } from "./routes";
-import path from "path";
+
 const app = express();
 
-app.use(express.static(path.join(__dirname,"..","public")));
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.set("views", path.join(__dirname, "..", "public"));
 app.engine("html", require("ejs").renderFile);
-app.set("view engine","html");
+app.set("view engine", "html");
 
-app.get("/pages/client", (request,response) =>{
-  return response.render("html/client.html");
+app.get("/pages/client", (request, response) => {
+    return response.render("html/client.html");
 });
 
-app.get("/pages/admin", (request,response) =>{
-  return response.render("html/admin.html");
+app.get("/pages/admin", (request, response) => {
+    return response.render("html/admin.html");
 });
 
-const http = createServer(app); // Criando o protocolo http
-const io = new Server(http); // Criando o protocolo wb - wesocket
+const http = createServer(app); // Criando protoloco http
+const io = new Server(http); // Criando protocolo ws
 
-io.on("connection", (socket : Socket) => {
-  console.log("Se consectou", socket.id);
+io.on("connection", (socket: Socket) => {
+    //console.log("Se conectou", socket.id);
 });
 
 app.use(express.json());
+
 app.use(routes);
 
-export {http,io}
+export { http, io };
--- src/websocket/admin.ts
+++ src/websocket/admin.ts
@@ -1,36 +1,46 @@
 import { io } from "../http";
 import { ConnectionsService } from "../services/ConnectionsService";
 import { MessagesService } from "../services/MessagesService";
+
 io.on("connect", async (socket) => {
-  const connectionsService = new ConnectionsService();
-  const messagesService = new MessagesService();
+    const connectionsService = new ConnectionsService();
+    const messagesService = new MessagesService();
 
-  const allConnectionWithoutAdmin = await connectionsService.findAllWithoutAdmin();
+    const allConnectionsWithoutAdmin = await connectionsService.findAllWithoutAdmin();
 
-  io.emit("admin_list_all_users", allConnectionWithoutAdmin);
+    io.emit("admin_list_all_users", allConnectionsWithoutAdmin);
 
-  socket.on("admin_list_message_by_user",async (params, callback) =>{
-    const { user_id } = params;
+    socket.on("admin_list_messages_by_user", async (params, callback) => {
+        const { user_id } = params;
 
-    const allMessages = await messagesService.listByUser(user_id);
-    callback(allMessages);
-  });
+        const allMessages = await messagesService.listByUser(user_id);
 
-  socket.on("admin_send_message", async params => {
-    const { user_id, text} = params;
-    
-    await messagesService.create({
-      text,
-      user_id,
-      admin_id: socket.id,
+        callback(allMessages);
     });
 
-    const {socket_id} = await connectionsService.findByUserId(user_id);
+    socket.on("admin_send_message", async (params) => {
+        const { user_id, text } = params;
 
-    io.to(socket_id).emit("admin_send_to_client",{
-      text,
-      socket_id: socket.id
+        await messagesService.create({
+            text,
+            user_id,
+            admin_id: socket.id
+        });
+
+        const { socket_id }= await connectionsService.findByUserId(user_id);
+
+        io.to(socket_id).emit("admin_send_to_client", {
+            text,
+            socket_id: socket.id
+        })
     });
-    
-  });
+
+    // socket.on("admin_user_in_support", async (params) => {
+    //     const { user_id } = params;
+    //     await connectionsService.updateAdminID(user_id, socket.id);
+
+    //     const allConnectionsWithoutAdmin = await connectionsService.findAllWithoutAdmin();
+
+    //     io.emit("admin_list_all_users", allConnectionsWithoutAdmin);
+    // });
 });
